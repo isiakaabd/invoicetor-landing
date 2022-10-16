5376643960b91db187165710e29964cff7ba4d05
Remlad <isiakaabd@gmail.com>
2022-10-16 14:10:33 +0100

    added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 ReactDOM.render(
   <StrictMode>
@@ -23,4 +23,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
